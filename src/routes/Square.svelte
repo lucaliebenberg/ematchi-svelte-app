6181f094300980bb04c3051621c501f001ee3bd0
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-07-01 11:52:16 +0200

    project update

--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,10 +1,21 @@
-<div class="square">
+<script lang="ts">
+    export let emoji: string;
+</script>
 
+<div class="square">
+    <span>{emoji}</span>
 </div>
 
 
 <style>
     .square {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         background: yellow;
+    }
+
+    span { 
+        font-size: 5em
     }
 </style>
